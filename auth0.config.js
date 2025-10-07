8117e5cd6298b070a9fb94e79b0ddc6f40411420
rubenelhore <ruben.elhore@gmail.com>
2025-10-07 12:34:04 -0600

    Fix Auth0 cookie configuration for state parameter

--- auth0.config.js
+++ auth0.config.js
@@ -5,11 +5,18 @@ module.exports = {
     logout: '/auth/logout',
   },
   session: {
+    name: 'appSession',
+    rolling: true,
     rollingDuration: 60 * 60 * 24, // 1 day
     absoluteDuration: 60 * 60 * 24 * 7, // 7 days
     cookie: {
+      domain: process.env.NODE_ENV === 'production' ? 'ai-chatbot-v2-three.vercel.app' : undefined,
+      path: '/',
+      transient: false,
+      httpOnly: true,
+      secure: true,
       sameSite: 'lax',
-      secure: process.env.NODE_ENV === 'production',
     },
   },
+  auth0Logout: true,
 };
